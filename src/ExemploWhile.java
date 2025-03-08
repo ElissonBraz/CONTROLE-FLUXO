8380821020b565e9d6f9fe11dc66fa0e530ae579
Elisson Braz <59119081+ElissonBraz@users.noreply.github.com>
2025-03-08 12:42:10 -0300

    Add: Exercicíos DIO

---
+++ src/ExemploWhile.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExemploWhile {
+    private static double valorAleatorio(){
+        return ThreadLocalRandom.current().nextDouble(2, 15);
+    }
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        double mesada;
+
+        System.out.println("==============================");
+        System.out.println("Bom dia! Digite o valor que Joãozinho pode ter para comprar doces: ");
+        mesada = leia.nextDouble();
+
+        while (mesada > 0){
+            double valorDoce = valorAleatorio();
+
+            if (valorDoce > mesada) {
+                valorDoce = mesada;
+            }
+            System.out.println("Doce do valor: R$ " + String.format( "%.2f",valorDoce )+ " Adicionado no carrinho");
+            mesada = mesada - valorDoce;
+        }
+        System.out.println("Mesada: R$ " + mesada);
+        System.out.println("Joãozinho gastou toda a sua mesada em doces");
+    }
+}
